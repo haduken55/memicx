27b291b5b517aa808b37ac1e81cbb3b8013df006
haduken55 <110445980+haduken55@users.noreply.github.com>
2022-10-11 20:07:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = 'NjIwNDA2MzY1MTQ3NjI3NTIz.XXaygw.klPIAqXszSjSVP3vJ6Qwe6riB9U';
+const token = 'MTAxNTI0NzU5ODE1NzgyNDAxMQ.GrA3gs.z_ppDk9mjRZrJwuyzNwvcrSr4tqfjygkzsRSfM';
 usedCommandRecently4 = new Set();
 
 client.on('ready', () =>{
